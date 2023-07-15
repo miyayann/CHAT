68d92c77c7851318085e833575ed8fb576aa1564
miya <sirotumesirokuma@gmail.com>
2023-07-15 16:14:23 +0900

    change

--- app/Context/Store.tsx
+++ app/Context/Store.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { supabase } from "@/utils/supabase";
 import { createContext, useContext, useEffect, useState, ReactNode} from "react";
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import './globals.css'
 import{UserContextProvider} from "../app/Context/Store"
 
